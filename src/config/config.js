1af15225bb1f3a68db33ab95326f78a5da3bc99c
wuxiaoqing <wuxiaoqing_818@163.com>
2021-06-03 22:46:09 +0800

    张大炮是傻逼

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: 吴晓晴
  * @Date: 2021-05-23 13:52:20
- * @LastEditTime: 2021-05-23 14:28:49
- * @FilePath: \webDevelopment\blogDev\jspang-blog\react-blog\wxq-blog\src\config\config.js
+ * @LastEditTime: 2021-06-02 22:21:03
+ * @FilePath: \webDevelopment\blogDev\jspang-blog\react-blog\admin\src\config\config.js
  */
 let baseUrl;
 let configUrl = "http://127.0.0.1:7001"
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV == 'development') {
 } else if (process.env.NODE_ENV == 'debug') {
     baseUrl = '';
 } else if (process.env.NODE_ENV == 'production') {
-    baseUrl = 'http://127.0.0.1:7001';
+    baseUrl = 'http://47.93.6.17:7001';
 }
 let config = {
     baseUrl,
